50fe2e24af2b593a4d4733500196dcdb11210e33
PorUnaCabeza <makpia@163.com>
2016-07-22 16:11:50 +0800

    compressBatch

--- src/main/java/util/ImageUtil.java
+++ src/main/java/util/ImageUtil.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.*;
 import java.util.Iterator;
 
@@ -16,6 +17,7 @@ import java.util.Iterator;
 public class ImageUtil {
 
     public static BufferedImage compressJpg(int compression, BufferedImage image) throws IOException {
+        int originSize = getImageSize(image,"jpg");
         Iterator<ImageWriter> i = ImageIO.getImageWritersByFormatName("jpeg");
         ImageWriter jpegWriter = i.next();
         ImageWriteParam param = jpegWriter.getDefaultWriteParam();
@@ -27,7 +29,8 @@ public class ImageUtil {
         jpegWriter.write(null, new IIOImage(image, null, null), param);
         jpegWriter.dispose();
         BufferedImage resultImage = ImageIO.read(new ByteArrayInputStream(bos.toByteArray()));
-        return resultImage;
+        int afterSize = getImageSize(resultImage,"jpg");
+        return originSize > afterSize ? resultImage : image;
     }
 
     public static BufferedImage splitCircle(BufferedImage image) {
@@ -52,5 +55,46 @@ public class ImageUtil {
         return false;
     }
 
+    public static void saveAsPng(BufferedImage image, String fileName) throws IOException {
+        ImageIO.write(image, "png", new File(fileName));
+        byte[] data = ((DataBufferByte) image.getData().getDataBuffer()).getData();
+    }
 
+    public static void saveAsJpg(BufferedImage image, String fileName) throws IOException {
+        ImageIO.write(image, "jpg", new File(fileName));
+    }
+
+    public static int getImageSize(BufferedImage image,String format) throws IOException {
+        if(image==null)
+            return 0;
+        ByteArrayOutputStream bos=new ByteArrayOutputStream(255);
+        ImageIO.write(image,format,bos);
+        return bos.toByteArray().length;
+    }
+
+    public static boolean isJpg(String fileName){
+        return fileName.indexOf(".jpg")>0;
+    }
+
+    public static void compressBatch(String originPath, String newPath) throws IOException {
+        File originDir = new File(originPath);
+        File newDir = new File(newPath);
+        if (!originDir.exists() && !originDir.isDirectory()) {
+            System.out.println("路径不对");
+            return;
+        }
+        if (!newDir.exists() && !newDir.isDirectory()) {
+            System.out.println("路径不存在，现创建");
+            newDir.mkdir();
+        }
+        File[] files = originDir.listFiles();
+        for (File file : files) {
+            BufferedImage image=ImageIO.read(file);
+            if(isJpg(file.getName())&&getImageSize(image,"jpg")>0){
+                String fileName=file.getName();
+                System.out.println("压缩"+fileName);
+                saveAsJpg(compressJpg(6, image), newPath + "\\" + fileName);
+            }
+        }
+    }
 }
--- src/test/java/ImageUtilTest.java
+++ src/test/java/ImageUtilTest.java
@@ -13,7 +13,7 @@ public class ImageUtilTest {
 
     @Test
     public void testCompress() throws IOException {
-        BufferedImage bi = ImageIO.read(new File("C:\\蓝天草地.jpg"));
+        BufferedImage bi = ImageIO.read(new File("E:\\merchantShow\\154572115184582656.jpg"));
         ImageIO.write(ImageUtil.compressJpg(5, bi), "jpg", new File("c:\\1.jpg"));
     }
 
@@ -21,5 +21,21 @@ public class ImageUtilTest {
     public void testSplitCircle() throws IOException {
         BufferedImage bi = ImageIO.read(new File("C:\\头像.jpg"));
         ImageIO.write(ImageUtil.splitCircle(bi), "png", new File("c:\\2.png"));
+    }
+
+    @Test
+    public void testIsJpg(){
+        System.out.println(ImageUtil.isJpg("166435962073776128.jpg"));
+    }
+
+    @Test
+    public void testCompressBatch() throws IOException {
+        ImageUtil.compressBatch("E:\\merchantShow","E:\\compress_result");
+    }
+
+    @Test
+    public void testGetImageSize() throws IOException {
+       int size= ImageUtil.getImageSize(ImageIO.read(new File("E:\\merchantShow\\154572141344456704.jpg")),"jpg");
+        System.out.println(size);
     }
 }
